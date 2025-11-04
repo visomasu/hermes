12c202a608109563292673784d7ff307da38f6b7
Vignesh Somasundaram <visomasu@microsoft.com>
2025-11-03 16:29:14 -0800

    Create database and container on initialization.

--- Hermes/DI/StorageModule.cs
+++ Hermes/DI/StorageModule.cs
@@ -30,9 +30,9 @@ namespace Hermes.DI
             if (_environment.IsDevelopment())
             {
                 // Local CosmosDB emulator connection string
-                connectionString = "AccountEndpoint=https://localhost:8081/;AccountKey=\"C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5Qj4=\";";
-                databaseId = "LocalEmulatorDb";
-                containerId = "LocalEmulatorContainer";
+                connectionString = "AccountEndpoint=https://localhost:8081/;AccountKey=\"C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==\";";
+                databaseId = "HermesDb";
+                containerId = "Hermes";
             }
             else
             {
